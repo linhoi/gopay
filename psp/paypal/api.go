8ee1da7b343fd0e677b2b1583dfceb8ecc9843d2
linhui <hui.lin@mihoyo.com>
2022-01-09 16:29:59 +0800

    feat:doc

--- psp/paypal/api.go
+++ psp/paypal/api.go
@@ -11,10 +11,11 @@ type API interface {
 	// GetAccessToken AccessToken ...
 	GetAccessToken(ctx context.Context) (string, error)
 
-
+	// 争议查询
 	ListDisputes(ctx context.Context) (disputeItems []DisputeItem, error error)
 	ShowDisputeDetails(ctx context.Context, disputeID string) (detail ShowDisputeDetailsResp, error error)
 
+	// 交易查询
 	//https://developer.paypal.com/docs/api/transaction-search/v1/#transactions_get
 	GetTransaction(ctx context.Context, transactionID string, startTime, endTime time.Time) (transaction TransactionInfo, err error)
 	GetRefundTransaction(ctx context.Context, startTime, endTime time.Time) (transactions []TransactionInfo, err error)
@@ -22,10 +23,7 @@ type API interface {
 	//NVP
 	SetExpressCheckout(ctx context.Context, req SetExpressCheckoutReq) (SetExpressCheckoutResp , error)
 	GetExpressCheckoutDetails(ctx context.Context, req GetExpressCheckoutDetailsReq) (GetExpressCheckoutDetailsResp , error)
-
 	DoExpressCheckoutPayment(ctx context.Context, req GetExpressCheckoutDetailsReq) (GetExpressCheckoutDetailsResp , error)
-
-
 }
 
 
